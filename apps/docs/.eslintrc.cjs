b77ace40b930f0724eadedec9b87e417fbb15352
Marshall Ku <marshall@kakao.com>
2024-01-23 21:02:23 +0900

    Initialize project

---
+++ apps/docs/.eslintrc.cjs
@@ -0,0 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    extends: ["@marshallku/eslint-config/storybook.js"],
+};
---
+++ packages/ui/.eslintrc.js
@@ -0,0 +1,4 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+    extends: ["@marshallku/eslint-config/react.js"],
+};
