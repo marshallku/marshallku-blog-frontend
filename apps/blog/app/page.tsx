318c872b6f6fca1f937a2d0b82401d17ee5893b1
Marshall Ku <marshall@kakao.com>
2024-01-24 23:34:19 +0900

    Add recent posts and categories in home

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -1,7 +1,26 @@
+import { getAllCategories, getPosts } from "#utils";
+import { PAGE_SIZE } from "#constants";
+import Link from "next/link";
+
 export default async function Home() {
+    const posts = getPosts();
+    const postsInPage = posts.slice(0, PAGE_SIZE);
+
     return (
         <section>
-            <h1>Hi there!</h1>
+            <h1>Welcome to my blog</h1>
+            <h2>Recent posts</h2>
+            {postsInPage.map(({ data, slug }) => (
+                <article key={slug}>
+                    <Link href={slug}>
+                        <h2>{data.title}</h2>
+                    </Link>
+                </article>
+            ))}
+            <h2>Categories</h2>
+            {getAllCategories().map((category) => (
+                <Link href={category}>{category}</Link>
+            ))}
         </section>
     );
 }
