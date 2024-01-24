be526d893ecc452276e056abd5f0d9965df383cc
Marshall Ku <marshall@kakao.com>
2024-01-24 12:32:30 +0900

    Add archive index page

---
+++ apps/blog/app/[category]/page.tsx
@@ -0,0 +1,58 @@
+import { PAGE_SIZE } from "#constants";
+import { getPosts } from "#utils";
+
+export const dynamic = "error";
+
+interface ArchiveProps {
+    params: {
+        category: string;
+        index: string;
+    };
+}
+
+export function generateStaticParams() {
+    const posts = getPosts();
+    const groupedPosts = posts.reduce(
+        (acc, post) => {
+            const category = post.category.slice(1);
+
+            if (!category) {
+                return acc;
+            }
+
+            if (!acc[category]) {
+                acc[category] = [];
+            }
+
+            acc[category].push(post.slug);
+            return acc;
+        },
+        {} as Record<string, string[]>,
+    );
+
+    console.log(
+        Object.entries(groupedPosts).flatMap(([category]) => ({
+            category,
+        })),
+    );
+
+    return Object.entries(groupedPosts).flatMap(([category]) => ({
+        category,
+    }));
+}
+
+export default async function Archive({ params: { category } }: ArchiveProps) {
+    const posts = getPosts(category);
+    const postsInPage = posts.slice(PAGE_SIZE);
+
+    return (
+        <section>
+            <h2>Recent posts</h2>
+            <section>
+                {postsInPage.map(({ content, data }, i) => (
+                    <article key={i}>{JSON.stringify({ content, data })}</article>
+                ))}
+            </section>
+        </section>
+    );
+}
