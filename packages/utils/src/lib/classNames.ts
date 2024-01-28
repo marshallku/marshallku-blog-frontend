561b9954994556c07ba35fae598a1f410eeabc04
Marshall Ku <marshall@kakao.com>
2024-01-28 15:40:00 +0900

    Implement class name formatter

---
+++ packages/utils/src/lib/classNames.ts
@@ -0,0 +1,57 @@
+export default function classNames(styles: Record<string, string> = {}, rootClassName = "") {
+    const hasOwn = {}.hasOwnProperty;
+    const func = (...names: unknown[]): string => {
+        const classes: unknown[] = [];
+
+        if (names.length === 0) {
+            // It's root
+            classes.push(styles[rootClassName] || rootClassName);
+        }
+
+        for (let i = 0, max = names.length; i < max; i++) {
+            const name = names[i];
+
+            // HACK: To add extra modifier in root
+            if (name === "") {
+                classes.push(styles[rootClassName] || rootClassName);
+            }
+
+            if (!name) {
+                continue;
+            }
+
+            const nameType = typeof name;
+
+            if (nameType === "string" || nameType === "number") {
+                const nameWithRoot = rootClassName + name;
+                classes.push(styles[nameWithRoot] || nameWithRoot);
+                continue;
+            }
+
+            if (Array.isArray(name)) {
+                func(name);
+                continue;
+            }
+
+            if (nameType === "object") {
+                // It's not null and array, so it must be an object.
+                for (const key in name as Record<string, unknown>) {
+                    const keyWithRoot = rootClassName + key;
+
+                    if (key === "className" && (name as Record<string, unknown>)[key]) {
+                        classes.push((name as Record<string, unknown>)[key]);
+                        continue;
+                    }
+
+                    if (hasOwn.call(name, keyWithRoot) && (name as Record<string, unknown>)[keyWithRoot]) {
+                        classes.push(styles[keyWithRoot] || keyWithRoot);
+                    }
+                }
+            }
+        }
+
+        return classes.join(" ");
+    };
+
+    return func;
+}
