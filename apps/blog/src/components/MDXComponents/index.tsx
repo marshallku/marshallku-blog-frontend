1467eee8ddfdfacc65eb42fed00425f53ff7e5b7
Marshall Ku <marshall@kakao.com>
2024-01-30 22:44:45 +0900

    Modify design of single post page

--- apps/blog/src/components/MDXComponents/index.tsx
+++ apps/blog/src/components/MDXComponents/index.tsx
@@ -2,7 +2,7 @@ import { HTMLProps } from "react";
 import NextImage from "next/image";
 import NextLink from "next/link";
 
-function Image({ src, alt, width, height }: HTMLProps<HTMLImageElement>) {
+function Image({ src, alt, width, height, title }: HTMLProps<HTMLImageElement>) {
     if (!src) {
         return null;
     }
@@ -11,7 +11,12 @@ function Image({ src, alt, width, height }: HTMLProps<HTMLImageElement>) {
         return <img src={src} alt={alt} width={width} height={height} />;
     }
 
-    return <NextImage src={src} alt={alt || ""} width={Number(width)} height={Number(height)} />;
+    return (
+        <figure className="image-block">
+            <NextImage src={src} alt={alt || ""} width={Number(width)} height={Number(height)} />
+            {title && <figcaption>{title}</figcaption>}
+        </figure>
+    );
 }
 
 function Link({ href, ...rest }: Omit<HTMLProps<HTMLAnchorElement>, "ref">) {
@@ -21,6 +26,10 @@ function Link({ href, ...rest }: Omit<HTMLProps<HTMLAnchorElement>, "ref">) {
 
     if (href.startsWith("#")) {
         return <a href={href} {...rest} />;
+    }
+
+    if (href.startsWith("http")) {
+        return <a className="external-link" href={href} {...rest} target="_blank" rel="noopener noreferrer" />;
     }
 
     return <NextLink data-foo="id" href={href} {...rest} />;
