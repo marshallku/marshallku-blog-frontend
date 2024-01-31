735b356a72464649107384cb32c0a0afc13d3cec
Marshall Ku <marshall@kakao.com>
2024-01-31 12:44:37 +0900

    Add tag archive pages

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -3,6 +3,8 @@ import { PostList } from "#components";
 import { getAllCategories, getPosts } from "#utils";
 import { PAGE_SIZE } from "#constants";
 
+export const dynamic = "error";
+
 export default async function Home() {
     const posts = getPosts();
     const postsInPage = posts.slice(0, PAGE_SIZE);
---
+++ apps/blog/app/tag/[tag]/page.tsx
@@ -0,0 +1,32 @@
+import { PostList } from "#components";
+import { getTags, getPostsByTag } from "#utils";
+import { PAGE_SIZE } from "#constants";
+
+export const dynamic = "error";
+
+interface TagArchivePageProps {
+    params: { tag: string };
+}
+
+export async function getStaticParams() {
+    return getTags().map((tag) => ({ tag }));
+}
+
+export default function TagArchivePage({ params: { tag } }: TagArchivePageProps) {
+    const posts = getPostsByTag(decodeURIComponent(tag));
+    const postsInPage = posts.slice(0, PAGE_SIZE);
+
+    return (
+        <section>
+            <h1>{tag} 태그 글</h1>
+            <PostList
+                posts={postsInPage}
+                paginationProps={{
+                    currentIndex: 1,
+                    totalCount: posts.length,
+                    basePath: `/tag/${tag}`,
+                }}
+            />
+        </section>
+    );
+}
