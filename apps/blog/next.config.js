38f0f40c45e57a4e514988d24500bdfd16ad4fc0
Marshall Ku <marshall@kakao.com>
2024-02-01 07:39:28 +0900

    Modify output to standalone

--- apps/blog/next.config.js
+++ apps/blog/next.config.js
@@ -8,6 +8,7 @@ const analyzing = process.env.ANALYZE === "true";
 const nextConfig = {
     reactStrictMode: true,
     transpilePackages: ["@marshallku/ui"],
+    output: "standalone",
     experimental: {
         outputFileTracingRoot: path.join(__dirname, "../../"),
     },
