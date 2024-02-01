5da5103cc920aade90d612702d4b836ff56ac4d0
Marshall Ku <marshall@kakao.com>
2024-02-01 20:13:19 +0900

    Modify title of archive pages

--- apps/blog/app/[category]/page/[index]/page.tsx
+++ apps/blog/app/[category]/page/[index]/page.tsx
@@ -41,7 +41,7 @@ export default async function ArchivePage({ params: { category, index } }: Archi
 
     return (
         <Archive
-            title={categoryInfo?.name || "카테고리 최근 글"}
+            title={categoryInfo?.name ? `${categoryInfo.name} 카테고리 글` : "카테고리 최근 글"}
             coverImage={categoryInfo?.coverImage || postsInPage[0]?.data?.coverImage}
             postListProps={{
                 posts: postsInPage,
