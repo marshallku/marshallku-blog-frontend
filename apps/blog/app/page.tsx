94db4f4e7e5ec12c3c39508c04d1bc1b3c3c556c
Marshall Ku <marshall@kakao.com>
2024-02-02 21:19:08 +0900

    Update design of front page

--- apps/blog/app/page.tsx
+++ apps/blog/app/page.tsx
@@ -1,23 +1,56 @@
 import Link from "next/link";
-import { PostList } from "#components";
-import { getAllCategories, getPosts } from "#utils";
+import { classNames } from "@marshallku/utils";
+import { PostList, PostListGallery, Typography } from "#components";
+import { getCategories, getPosts } from "#utils";
 import { PAGE_SIZE } from "#constants";
+import styles from "./page.module.scss";
+import { Icon } from "@marshallku/icon";
 
 export const dynamic = "error";
 
+const cx = classNames(styles, "home");
+
 export default async function Home() {
     const posts = getPosts();
-    const postsInPage = posts.slice(0, PAGE_SIZE);
+    const mostRecentPost = posts[0];
+    const postsInPage = posts.slice(1, PAGE_SIZE);
+    const galleryPosts = posts.filter(({ category }) => category === "/gallery").slice(0, 6);
 
     return (
-        <section>
-            <h1>Welcome to my blog</h1>
-            <h2>Recent posts</h2>
-            <PostList posts={postsInPage} />
-            <h2>Categories</h2>
-            {getAllCategories().map((category) => (
-                <Link href={category}>{category}</Link>
-            ))}
+        <section className={cx()}>
+            <header className={cx("-header")}>
+                <figure className={cx("-header__cover")}>
+                    <img src={mostRecentPost.data.coverImage} alt={mostRecentPost.data.title} />
+                </figure>
+                <div className={cx("-header__content")}>
+                    <Typography variant="c1">{mostRecentPost.data.date.posted.toLocaleDateString("ko-KR")}</Typography>
+                    <Typography component="h1" variant="h1" fontWeight={700} className={cx("-header__title")}>
+                        <Link href={mostRecentPost.slug}>{mostRecentPost.data.title}</Link>
+                    </Typography>
+                    <Typography variant="b2">
+                        <Link href={mostRecentPost.slug}>{mostRecentPost.data.description}</Link>
+                    </Typography>
+                </div>
+            </header>
+            <section className={cx("__container")}>
+                <PostList posts={postsInPage} />
+            </section>
+            <section className={cx("__container", "__container--padding", "__category")}>
+                {getCategories()
+                    .filter(({ icon }) => !!icon)
+                    .map(({ slug, name, icon, color }) => (
+                        <Link href={slug} key={slug}>
+                            <Typography variant="h4" fontWeight={700}>
+                                {name}
+                            </Typography>
+                            <Icon name={icon!} color={color} />
+                            <div style={{ backgroundColor: color }} />
+                        </Link>
+                    ))}
+            </section>
+            <section className={cx("__gallery")}>
+                <PostListGallery posts={galleryPosts} />
+            </section>
         </section>
     );
 }
--- apps/blog/src/components/index.ts
+++ apps/blog/src/components/index.ts
@@ -1,10 +1,11 @@
 export { default as Banner, type BannerProps } from "./Banner";
-export { default as GlobalNavigation, type GlobalNavigationProps } from "./GlobalNavigation";
+export { default as GlobalNavigation } from "./GlobalNavigation";
 export { default as Logo, type LogoProps } from "./Logo";
 export { default as MDXComponents } from "./MDXComponents";
 export { default as Pagination, type PaginationProps } from "./Pagination";
 export { default as PostCard, type PostCardProps } from "./PostCard";
 export { default as PostList, type PostListProps } from "./PostList";
+export { default as PostListGallery, type PostListGalleryProps } from "./PostListGallery";
 export { default as PrevNextPost, type PrevNextPostProps } from "./PrevNextPost";
 export { default as ThemeToggle } from "./ThemeToggle";
 export {
