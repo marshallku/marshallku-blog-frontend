f47dca20d0167245d4c72dfa795e6455440b52b4
Marshall Ku <marshall@kakao.com>
2024-02-04 10:59:33 +0900

    Modify design of InteractPost

--- apps/blog/src/components/InteractPost/index.tsx
+++ apps/blog/src/components/InteractPost/index.tsx
@@ -5,6 +5,8 @@ import { Icon } from "@marshallku/icon";
 import { classNames } from "@marshallku/utils";
 import { toast } from "@marshallku/toast";
 import styles from "./index.module.scss";
+import Button from "#components/Button";
+import { AnchorHTMLAttributes } from "react";
 
 export interface InteractPostProps {
     className?: string;
@@ -19,7 +21,8 @@ function InteractPost({ className, title, slug }: InteractPostProps) {
     const url = `https://marshallku.com/${slug}`;
     return (
         <div className={cx("", { className })}>
-            <a
+            <Button
+                component={(x: AnchorHTMLAttributes<HTMLAnchorElement>) => <a {...x} />}
                 href={`https://github.com/marshallku/marshallku-blog-frontend/issues/new?title=${encodeURIComponent(
                     `Discussion about ${title}`,
                 )}&assignees=marshallku&body=${encodeURIComponent(`Link of the post: ${url}\n\n---\n\n`)}`}
@@ -29,10 +32,9 @@ function InteractPost({ className, title, slug }: InteractPostProps) {
             >
                 <Icon name="github" />
                 Report an issue
-            </a>
+            </Button>
             <div className={cx("__send")}>
-                <button
-                    type="button"
+                <Button
                     onClick={() => {
                         copyToClipboard(url);
                         toast("주소가 클립보드에 복사되었습니다.", { removable: true });
@@ -40,7 +42,7 @@ function InteractPost({ className, title, slug }: InteractPostProps) {
                 >
                     <Icon name="link" />
                     <span className="sr-only">Copy link</span>
-                </button>
+                </Button>
             </div>
         </div>
     );
