93a17e8c91d6903004105874c319792a3d74c7b0
Marshall Ku <marshall@kakao.com>
2024-02-04 14:18:23 +0900

    Add error pages

---
+++ apps/blog/app/error.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import { useEffect } from "react";
+import Error from "#templates/Error";
+
+export default function ErrorPage({ error, reset }: { error: Error & { digest?: string }; reset: () => void }) {
+    useEffect(() => {
+        console.error(error);
+    }, [error]);
+
+    return <Error reset={reset} />;
+}
