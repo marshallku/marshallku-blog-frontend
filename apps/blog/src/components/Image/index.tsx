ecdac1fb2c68cff61fc8860719ec6add826ba4cd
Marshall Ku <marshall@kakao.com>
2024-02-09 18:24:24 +0900

    Fix Image to preserve extension of images

--- apps/blog/src/components/Image/index.tsx
+++ apps/blog/src/components/Image/index.tsx
@@ -1,21 +1,28 @@
 import { type ImgHTMLAttributes } from "react";
 
 interface ImageProps extends ImgHTMLAttributes<HTMLImageElement> {
+    src: string;
     forceSize?: number;
 }
 
 const IMAGE_SIZE = [480, 600, 860, 1180];
 
 function Image({ src, alt, width, height, forceSize, ...rest }: ImageProps) {
+    const extension = src.split(".").pop();
+    const srcWithoutExtension = src.split(".").slice(0, -1).join(".");
+
     return (
         <img
-            src={`${process.env.NEXT_PUBLIC_CDN_URL}${src}${
-                forceSize && process.env.NEXT_PUBLIC_CDN_URL !== "" ? `=w${forceSize}` : ""
-            }`}
+            src={`${process.env.NEXT_PUBLIC_CDN_URL}${srcWithoutExtension}${
+                forceSize && process.env.NEXT_PUBLIC_CDN_URL !== "" ? `.w${forceSize}` : ""
+            }.${extension}`}
             srcSet={
                 width && height && process.env.NEXT_PUBLIC_CDN_URL !== ""
                     ? IMAGE_SIZE.filter((size) => size < Number(width))
-                          .map((size) => `${process.env.NEXT_PUBLIC_CDN_URL}${src}=w${size} ${size}w`)
+                          .map(
+                              (size) =>
+                                  `${process.env.NEXT_PUBLIC_CDN_URL}${srcWithoutExtension}.w${size}.${extension} ${size}w`,
+                          )
                           .join(", ")
                     : undefined
             }
