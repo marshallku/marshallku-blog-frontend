eee1a173c5955c3376a924da652245812d63ef34
Marshall Ku <marshall@kakao.com>
2024-02-11 02:31:35 +0900

    Modify design of PrevNextPost

--- apps/blog/src/components/PrevNextPost/index.tsx
+++ apps/blog/src/components/PrevNextPost/index.tsx
@@ -4,6 +4,7 @@ import Image from "#components/Image";
 import Typography from "#components/Typography";
 import { Post } from "#types";
 import styles from "./index.module.scss";
+import { Icon } from "@marshallku/icon";
 
 export interface PrevNextPostProps {
     previousPost?: Post;
@@ -16,13 +17,14 @@ const Article = ({ post, isPreviousPost }: { post: Post; isPreviousPost?: boolea
     <article className={cx("-post", isPreviousPost && "-post--previous")}>
         <Link href={post.slug} className={cx("-post__meta")}>
             <Typography variant="c1" className={cx("-post__label")}>
+                <Icon name={isPreviousPost ? "arrow-back" : "arrow-forward"} />
                 {isPreviousPost ? "이전 글" : "다음 글"}
             </Typography>
-            <Typography variant="h2" component="h4" className={cx("-post__title")} fontWeight={600}>
+            <Typography variant="h4" component="h4" className={cx("-post__title")} fontWeight={600}>
                 {post.data.title}
             </Typography>
         </Link>
-        <Image className={cx("-post__cover-image")} src={post.data.coverImage} alt={post.data.title} />
+        <Image className={cx("-post__cover-image")} src={post.data.coverImage} alt={post.data.title} forceSize={500} />
     </article>
 );
 
