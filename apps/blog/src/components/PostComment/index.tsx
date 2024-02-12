c9aa7cef6617f4b0b126588ee92c6ca4d214b26d
Marshall Ku <marshall@kakao.com>
2024-02-13 00:09:40 +0900

    Remove dynamic data with hack

--- apps/blog/src/components/PostComment/index.tsx
+++ apps/blog/src/components/PostComment/index.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Suspense, useMemo } from "react";
+import { Suspense, useEffect, useMemo, useState } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ErrorBoundary } from "@marshallku/react-error-boundary";
 import { classNames } from "@marshallku/utils";
@@ -16,16 +16,26 @@ const cx = classNames(styles, "post-comment");
 
 function PostComment({ slug }: PostCommentProps) {
     const queryClient = useMemo(() => new QueryClient(), []);
+    const [render, setRender] = useState(false);
+
+    useEffect(() => {
+        setRender(true);
+    }, []);
 
     return (
         <div className={cx()}>
             <QueryClientProvider client={queryClient}>
-                <CommentForm slug={slug} />
-                <ErrorBoundary fallback={null}>
-                    <Suspense fallback={null}>
-                        <CommentList slug={slug} />
-                    </Suspense>
-                </ErrorBoundary>
+                {/* HACK: Removing dynamic data while building application */}
+                {render && (
+                    <>
+                        <CommentForm slug={slug} />
+                        <ErrorBoundary fallback={null}>
+                            <Suspense fallback={null}>
+                                <CommentList slug={slug} />
+                            </Suspense>
+                        </ErrorBoundary>
+                    </>
+                )}
             </QueryClientProvider>
         </div>
     );
