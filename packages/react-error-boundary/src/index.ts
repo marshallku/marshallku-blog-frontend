491612826dd95c4bfe9070b4852f2aec6d63d2bd
Marshall Ku <marshall@kakao.com>
2024-02-12 12:00:34 +0900

    Add error boundary package

---
+++ packages/react-error-boundary/src/index.ts
@@ -0,0 +1,9 @@
+"use client";
+
+export * from "./lib/ErrorBoundary";
+export * from "./lib/ErrorBoundaryContext";
+export * from "./lib/useErrorBoundary";
+export * from "./lib/withErrorBoundary";
+
+// TypeScript types
+export * from "./lib/types";
