4656a1cbc24b3dcaf0572c87178458a7cd75e7a7
Marshall Ku <marshall@kakao.com>
2024-02-12 13:50:24 +0900

    Add api and action for submitting comment

--- apps/blog/src/api/comment/api.ts
+++ apps/blog/src/api/comment/api.ts
@@ -1,6 +1,17 @@
 import { request } from "#api/instance";
-import { CommentListResponse } from "./types";
+import { Comment, CommentListResponse } from "./types";
 
 export async function getComments(slug: string) {
-    return request<CommentListResponse>(`/comment/list?postSlug=${encodeURIComponent(slug)}`);
+    return request<CommentListResponse>(`/comment/list?postSlug=${encodeURIComponent(slug)}`, {
+        next: {
+            tags: [slug],
+        },
+    });
+}
+
+export async function postComment(data: Omit<Comment, "_id" | "createdAt" | "byPostAuthor">) {
+    return request("/comment/create", {
+        method: "POST",
+        body: JSON.stringify(data),
+    });
 }
