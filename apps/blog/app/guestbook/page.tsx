3fcec64d069cdbff0bc10aefb6e460e345748d30
Marshall Ku <marshall@kakao.com>
2024-02-24 16:28:12 +0900

    Add guestbook page

---
+++ apps/blog/app/guestbook/page.tsx
@@ -0,0 +1,39 @@
+import { revalidateTag } from "next/cache";
+import { Suspense } from "react";
+import { classNames } from "@marshallku/utils";
+import { getComments, postComment } from "#api/comment/api.server";
+import CommentForm from "#components/CommentForm";
+import CommentList from "#components/CommentList";
+import Typography from "#components/Typography";
+import styles from "./page.module.scss";
+
+const SLUG = "/guestbook";
+
+const cx = classNames(styles, "guestbook");
+
+async function Comments() {
+    const data = await getComments(SLUG);
+    return <CommentList data={data} />;
+}
+
+export default async function GuestbookPage() {
+    return (
+        <div className={cx()}>
+            <Typography variant="h1" component="h1" className={cx("__title")} fontWeight={700}>
+                방명록
+            </Typography>
+            <CommentForm
+                slug={SLUG}
+                submit={async (data) => {
+                    "use server";
+
+                    await postComment(data);
+                    revalidateTag(SLUG);
+                }}
+            />
+            <Suspense fallback={<p>Loading...</p>}>
+                <Comments />
+            </Suspense>
+        </div>
+    );
+}
