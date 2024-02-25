6338eb14baf919f0463afd14444bc25d4df86f47
Marshall Ku <marshall@kakao.com>
2024-02-25 09:41:29 +0900

    Remove unnecessary eslint rules

--- apps/docs/.eslintrc.cjs
+++ apps/docs/.eslintrc.cjs
@@ -1,4 +1,8 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
     extends: ["@marshallku/eslint-config/storybook.js"],
+    rules: {
+        "import/no-extraneous-dependencies": "off",
+        "unicorn/filename-case": "off",
+    },
 };
