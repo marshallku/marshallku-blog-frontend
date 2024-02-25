80f89250e0626f5d9a756f660e557a98895e2660
Marshall Ku <marshall@kakao.com>
2024-02-25 09:42:06 +0900

    Add story for Button component

---
+++ apps/docs/stories/Button.stories.tsx
@@ -0,0 +1,24 @@
+import { type Meta, type StoryObj } from "@storybook/react";
+import Button, { type ButtonProps } from "@blog/Button";
+
+const story: Meta<ButtonProps> = {
+    component: Button,
+    title: "Components/Button",
+    tags: ["autodocs"],
+    parameters: {
+        docs: {
+            description: {
+                component: "Allows for user interaction and can trigger specific actions",
+            },
+        },
+    },
+};
+
+export default story;
+
+export const StringChildren: StoryObj<ButtonProps> = {
+    args: {
+        children: "Click me!",
+        disabled: false,
+    },
+};
