8b80712491d0388ac6ef2f9086463e24269a0848
Marshall Ku <marshall@kakao.com>
2024-02-28 07:50:05 +0900

    Add default settings for backstop

---
+++ apps/visual-regression/server.js
@@ -0,0 +1,20 @@
+const http = require("http");
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 8083;
+
+const filePath = path.join("..", "docs", "storybook-static");
+
+app.use(express.static(filePath));
+
+app.use("*", (_, res) => {
+    res.sendFile(path.join(filePath, "index.html"));
+});
+
+const server = http.createServer(app);
+
+server.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
