6e83c87a217825f9e18557b0b4013e0edf0197c6
Marshall Ku <marshall@kakao.com>
2024-05-08 16:04:33 +0900

    Fix typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,7 +35,7 @@ response=$(curl -s -o /dev/null -I -w "%{http_code}" "$SERVER_URL")
 
 if [[ $response -eq 200 ]]; then
     # Shift traffic
-    echo "$SERVER_URL responsed with status 200, deploying $ACTIVE_SERVICE"
+    echo "$SERVER_URL responded with status 200, deploying $ACTIVE_SERVICE"
     sudo /bin/sed -i "s/:$INACTIVE_PORT/:$ACTIVE_PORT/g" "$CONFIG_FILE"
     # Should prevent sigkill
     sudo /bin/systemctl restart nginx || exit 1
