c0f6646bda786fcb3abc81b1a9124af7982a0f3e
Marshall Ku <marshall@kakao.com>
2024-05-17 13:25:54 +0900

    Modify framework of storybook

---
+++ apps/docs/.storybook/preview.ts
@@ -0,0 +1,18 @@
+import { Preview } from "@storybook/react";
+import { getRouter } from "@storybook/nextjs/router.mock";
+import "#styles/reset.scss";
+import "#styles/globals.scss";
+import "../../blog/app/globals.css";
+
+const preview: Preview = {
+    parameters: {
+        nextjs: {
+            router: {},
+        },
+    },
+    async beforeEach() {
+        getRouter().push.mockImplementation(() => {});
+    },
+};
+
+export default preview;
