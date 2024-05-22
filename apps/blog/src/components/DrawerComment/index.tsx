a3927cfd7314b3cadd5384656ec231b250eecd84
Marshall Ku <marshall@kakao.com>
2024-05-23 08:31:23 +0900

    Add component for displaying components at drawer

---
+++ apps/blog/src/components/DrawerComment/index.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { classNames, formatDate } from "@marshallku/utils";
+import { useComments } from "#api/comment/queries";
+import CommentAvatar from "#components/CommentAvatar";
+import Typography from "#components/Typography";
+import styles from "./index.module.scss";
+
+const cx = classNames(styles, "drawer-comment");
+
+function DrawerComment() {
+    const { data } = useComments();
+
+    return (
+        <ol className={cx()}>
+            {data.map(({ name, url, body, createdAt, byPostAuthor, _id: id }) => (
+                <li key={id} className={cx("__comment")}>
+                    <figure className={cx("__avatar")}>
+                        <CommentAvatar name={name} url={url} postAuthor={byPostAuthor} />
+                    </figure>
+                    <div>
+                        <Typography className={cx("__body")} marginBottom={4}>
+                            {body}
+                        </Typography>
+                        <Typography variant="c2">
+                            {name} &middot;{" "}
+                            <time dateTime={new Date(createdAt).toISOString()}>
+                                {formatDate(new Date(createdAt), "yyyy. MM. dd")}
+                            </time>
+                        </Typography>
+                    </div>
+                </li>
+            ))}
+        </ol>
+    );
+}
+
+export default DrawerComment;
