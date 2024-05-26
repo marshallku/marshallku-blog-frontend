be7b72ded8d57a2c202b85a4c6148067220d8da9
Marshall Ku <marshall@kakao.com>
2024-05-27 08:09:48 +0900

    Add util for stringify query params

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./lib/array";
 export { default as classNames } from "./lib/classNames";
 export * from "./lib/date";
+export * from "./lib/query";
 export { default as to } from "./lib/to";
