0621c07c29ee7d34351b0171e6e952ea7f05983a
Marshall Ku <marshall@kakao.com>
2024-05-27 08:22:15 +0900

    Add link for navigating to post

--- apps/blog/src/components/DrawerComment/index.tsx
+++ apps/blog/src/components/DrawerComment/index.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import Link from "next/link";
 import { classNames, formatDate } from "@marshallku/utils";
 import { useComments } from "#api/comment/queries";
 import CommentAvatar from "#components/CommentAvatar";
@@ -13,22 +14,24 @@ function DrawerComment() {
 
     return (
         <ol className={cx()}>
-            {data.map(({ name, url, body, createdAt, byPostAuthor, _id: id }) => (
+            {data.map(({ _id: id, name, url, body, createdAt, byPostAuthor, postSlug }) => (
                 <li key={id} className={cx("__comment")}>
-                    <figure className={cx("__avatar")}>
-                        <CommentAvatar name={name} url={url} postAuthor={byPostAuthor} />
-                    </figure>
-                    <div>
-                        <Typography className={cx("__body")} marginBottom={4}>
-                            {body}
-                        </Typography>
-                        <Typography variant="c2">
-                            {name} &middot;{" "}
-                            <time dateTime={new Date(createdAt).toISOString()}>
-                                {formatDate(new Date(createdAt), "yyyy. MM. dd")}
-                            </time>
-                        </Typography>
-                    </div>
+                    <Link href={postSlug} className={cx("__link")}>
+                        <figure className={cx("__avatar")}>
+                            <CommentAvatar name={name} url={url} postAuthor={byPostAuthor} />
+                        </figure>
+                        <div>
+                            <Typography className={cx("__body")} marginBottom={4}>
+                                {body}
+                            </Typography>
+                            <Typography variant="c2">
+                                {name} &middot;{" "}
+                                <time dateTime={new Date(createdAt).toISOString()}>
+                                    {formatDate(new Date(createdAt), "yyyy. MM. dd")}
+                                </time>
+                            </Typography>
+                        </div>
+                    </Link>
                 </li>
             ))}
         </ol>
