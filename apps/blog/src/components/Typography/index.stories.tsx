4baadf3a906ec8e7d253092bb045c6519bdf9e4a
Marshall Ku <marshall@kakao.com>
2024-05-29 08:33:43 +0900

    Add story of Typography

---
+++ apps/blog/src/components/Typography/index.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import Typography, { type TypographyProps } from ".";
+
+const story: Meta<TypographyProps> = {
+    component: Typography,
+    title: "Typography/Typography",
+    tags: ["autodocs"],
+    parameters: {
+        docs: {
+            description: {
+                component: "Displays text in a visually appealing way",
+            },
+        },
+    },
+};
+
+export default story;
+
+export const Default: StoryObj<TypographyProps> = {
+    args: {
+        children: "Hello World!",
+    },
+};
