c386fb2dace23a8039d28ee45509ac2cfc703cb1
Marshall Ku <marshall@kakao.com>
2024-05-30 19:01:14 +0900

    Use shared eslint config

--- apps/blog/.eslintrc.cjs
+++ apps/blog/.eslintrc.cjs
@@ -3,7 +3,7 @@ require("@marshallku/eslint-config/patch");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
     env: { browser: true, es2020: true },
-    extends: ["@marshallku/eslint-config", "@marshallku/eslint-config/mixins/react"],
+    extends: ["@marshallku/eslint-config", "@marshallku/eslint-config/mixins/next"],
     settings: {
         react: {
             version: "18.3",
--- packages/toast/.eslintrc.cjs
+++ packages/toast/.eslintrc.cjs
@@ -3,12 +3,7 @@ require("@marshallku/eslint-config/patch");
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
     env: { browser: true, es2020: true },
-    extends: ["@marshallku/eslint-config", "@marshallku/eslint-config/mixins/react"],
-    settings: {
-        react: {
-            version: "18.3",
-        },
-    },
+    extends: ["@marshallku/eslint-config"],
     parserOptions: {
         project: true,
         tsconfigRootDir: __dirname,
