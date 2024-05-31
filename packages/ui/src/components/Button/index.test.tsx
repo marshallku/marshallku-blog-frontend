c15457c1a35b13fd8ba22819dbf6e886429a9f87
Marshall Ku <marshall@kakao.com>
2024-05-31 16:06:14 +0900

    Verify logics of Button

--- packages/ui/src/components/Button/index.test.tsx
+++ packages/ui/src/components/Button/index.test.tsx
@@ -1,5 +1,6 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { AnchorHTMLAttributes, DetailedHTMLProps } from "react";
 import Button from ".";
 
 describe("Button", () => {
@@ -7,5 +8,33 @@ describe("Button", () => {
         render(<Button>Testing</Button>);
 
         expect(screen.getByText(/Testing/i)).toBeDefined();
+    });
+
+    it("should have a default variant of secondary", () => {
+        render(<Button data-testid="test">Testing</Button>);
+
+        expect(
+            [...screen.getByTestId("test").classList].filter((x) => x.includes("button--variant-secondary")),
+        ).toHaveLength(1);
+    });
+
+    it("should be rendered with custom component", () => {
+        render(
+            <Button
+                component={({
+                    children,
+                    ...props
+                }: DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnchorElement>, HTMLAnchorElement>) => (
+                    <a {...props}>{children}</a>
+                )}
+                href="/test"
+                data-testid="test"
+            >
+                Testing
+            </Button>,
+        );
+
+        expect(screen.getByTestId("test").tagName).toBe("A");
+        expect(screen.getByTestId("test").getAttribute("href")).toBe("/test");
     });
 });
