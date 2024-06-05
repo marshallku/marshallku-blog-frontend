04ab70b2069854d181c8e26ac4dbe77630cdc5e8
Marshall Ku <marshall@kakao.com>
2024-06-05 19:50:56 +0900

    Verify basic logics in Typography

---
+++ packages/ui/src/components/Typography/index.test.tsx
@@ -0,0 +1,25 @@
+import { describe, expect, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import Typography, { type TypographyProps } from ".";
+
+describe("Typography", () => {
+    const defaultProps: TypographyProps = {
+        component: "p",
+        variant: "b1",
+        fontWeight: 400,
+        children: "Test",
+    };
+
+    it("should render with default props", () => {
+        render(<Typography {...defaultProps} />);
+
+        expect(screen.getByText("Test")).toBeTruthy();
+        expect(screen.getByText("Test").tagName).toBe("P");
+    });
+
+    it("should render with a different component", () => {
+        render(<Typography {...defaultProps} component="h1" />);
+
+        expect(screen.getByText("Test").tagName).toBe("h1");
+    });
+});
