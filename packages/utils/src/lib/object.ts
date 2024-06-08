1f024862cefc2cebb8f1b7d5f5aecb07b053a6c9
Marshall Ku <marshall@kakao.com>
2024-06-08 16:16:24 +0900

    Fix types in removeNullish

--- packages/utils/src/lib/object.ts
+++ packages/utils/src/lib/object.ts
@@ -4,23 +4,21 @@ type RemoveNullish<T> = {
 
 export function removeNullish<T extends object>(obj: T): RemoveNullish<T> {
     const newObj = { ...obj };
-    const result = {} as RemoveNullish<T>;
+    const result: Partial<RemoveNullish<T>> = {};
 
     for (const key in newObj) {
         // eslint-disable-next-line eqeqeq
         if (newObj[key] != null) {
             if (typeof newObj[key] === "object" && !Array.isArray(newObj[key])) {
-                result[key as keyof RemoveNullish<T>] = removeNullish(newObj[key]) as NonNullable<
+                result[key as keyof T] = removeNullish(newObj[key] as object) as unknown as NonNullable<
                     RemoveNullish<T>[keyof RemoveNullish<T>]
                 >;
                 continue;
             }
 
-            result[key as keyof RemoveNullish<T>] = newObj[key] as NonNullable<
-                RemoveNullish<T>[keyof RemoveNullish<T>]
-            >;
+            result[key as keyof T] = newObj[key] as NonNullable<RemoveNullish<T>[keyof RemoveNullish<T>]>;
         }
     }
 
-    return result;
+    return result as RemoveNullish<T>;
 }
