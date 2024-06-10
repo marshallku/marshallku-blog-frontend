42dc17d54508149a4b8c9bf46ad5dae49648229c
Marshall Ku <marshall@kakao.com>
2024-06-10 10:32:28 +0900

    Add result of visual regression test with table

---
+++ apps/visual-regression/reportResult.js
@@ -0,0 +1,26 @@
+const fs = require("fs/promises");
+const path = require("path");
+
+async function main() {
+    const reportPath = path.resolve(__dirname, "backstop_data/bitmaps_test");
+    const directories = await fs.readdir(reportPath);
+    const lastDirectory = directories[directories.length - 1];
+    const report = JSON.parse(await fs.readFile(path.resolve(reportPath, lastDirectory, "report.json"), "utf-8"));
+    const sorted = report.tests.sort((a) => (a.status === "fail" ? -1 : 0));
+    const tableHeads = ["Component", "Story", "Success", "Viewport", "MisMatch Percentage"];
+    const tableData = [
+        tableHeads,
+        tableHeads.map(() => "-"),
+        ...sorted.map(({ pair, status }) => [
+            pair.label.split("--")[0],
+            pair.label.split("--")[1],
+            status === "fail" ? "❌" : "✅",
+            pair.viewportLabel,
+            pair.diff ? `${pair.diff.misMatchPercentage}%` : "100%(no reference)",
+        ]),
+    ];
+
+    console.log(tableData.map((row) => `| ${row.join(" | ")} |`).join("%0A"));
+}
+
+main();
