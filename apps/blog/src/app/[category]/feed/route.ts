83d73abbefd2db481a3f4e79c448a0ff8a0f5897
Marshall Ku <marshall@kakao.com>
2024-07-13 10:38:07 +0900

    Fix type in marked renderer

--- apps/blog/src/app/[category]/feed/route.ts
+++ apps/blog/src/app/[category]/feed/route.ts
@@ -7,7 +7,7 @@ export const dynamic = "error";
 export async function GET(_: Request, { params }: { params: { category: string } }) {
     const renderer = new marked.Renderer();
 
-    renderer.link = (href, title, text) => {
+    renderer.link = ({ href, title, text }) => {
         return `<a href="${href}" title="${title}" target="_blank" rel="noopener noreferrer">${text}</a>`;
     };
 
--- apps/blog/src/app/feed/route.ts
+++ apps/blog/src/app/feed/route.ts
@@ -7,7 +7,7 @@ export const dynamic = "error";
 export async function GET() {
     const renderer = new marked.Renderer();
 
-    renderer.link = (href, title, text) => {
+    renderer.link = ({ href, title, text }) => {
         return `<a href="${href}" title="${title}" target="_blank" rel="noopener noreferrer">${text}</a>`;
     };
 
