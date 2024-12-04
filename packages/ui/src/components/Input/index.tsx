82e9e8f0738a7666f737e4bb55cb333d6cfe004b
Marshall Ku <marshall@kakao.com>
2024-12-05 08:03:33 +0900

    Add children in Input

--- packages/ui/src/components/Input/index.tsx
+++ packages/ui/src/components/Input/index.tsx
@@ -1,4 +1,4 @@
-import { type InputHTMLAttributes, forwardRef } from "react";
+import { type InputHTMLAttributes, ReactNode, forwardRef } from "react";
 import { classNames } from "@marshallku/utils";
 import Typography from "../Typography";
 import styles from "./index.module.scss";
@@ -8,22 +8,26 @@ type InputVariant = "line" | "box";
 export interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
     variant?: InputVariant;
     label?: string;
+    children?: ReactNode;
 }
 
 const cx = classNames(styles, "input");
 
-const Input = forwardRef<HTMLInputElement, InputProps>(({ className, label, variant = "box", ...props }, ref) => {
-    return (
-        <label className={cx("", `--${variant}`, { className })}>
-            {!!label && (
-                <Typography variant="c1" className={cx("__label")} component="div">
-                    {label}
-                </Typography>
-            )}
-            <input className={cx("__input")} {...props} ref={ref} />
-        </label>
-    );
-});
+const Input = forwardRef<HTMLInputElement, InputProps>(
+    ({ className, label, variant = "box", children, ...props }, ref) => {
+        return (
+            <label className={cx("", `--${variant}`, children && "--has-child", { className })}>
+                {!!label && (
+                    <Typography variant="c1" className={cx("__label")} component="div">
+                        {label}
+                    </Typography>
+                )}
+                <input className={cx("__input")} {...props} ref={ref} />
+                {children && <div className={cx("__children")}>{children}</div>}
+            </label>
+        );
+    },
+);
 
 Input.displayName = "Input";
 
