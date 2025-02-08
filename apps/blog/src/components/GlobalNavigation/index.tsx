9dd7f2d8735f19a498114df5d343a5c091a78826
Marshall Ku <marshall@kakao.com>
2025-02-09 08:16:38 +0900

    Remove unnecessary navigation

--- apps/blog/src/components/GlobalNavigation/index.tsx
+++ apps/blog/src/components/GlobalNavigation/index.tsx
@@ -14,7 +14,6 @@ const cx = classNames(styles, "global-navigation");
 const NAVIGATION = [
     { path: "/", name: "Home" },
     { name: "Categories", subMenu: getCategories() } as const,
-    { path: "/tags", name: "Tags" },
     { path: "/about", name: "About" },
     { path: "/guestbook", name: "Guestbook" },
 ];
