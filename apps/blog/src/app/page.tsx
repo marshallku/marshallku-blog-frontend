f4735fe7d752b714e3edd533f90895824d35d016
Marshall Ku <marshall@kakao.com>
2025-02-17 13:33:11 +0900

    Add random banner in index page

--- apps/blog/src/app/page.tsx
+++ apps/blog/src/app/page.tsx
@@ -4,10 +4,11 @@ import { Icon } from "@marshallku/icon";
 import { classNames } from "@marshallku/utils";
 import PostList from "#components/PostList";
 import PostListGallery from "#components/PostListGallery";
-import { getPosts, getCategories } from "#utils/post";
-import { PAGE_SIZE, SITE_DESCRIPTION, SITE_NAME } from "#constants";
-import styles from "./page.module.scss";
 import Profile from "#components/Profile";
+import { getPosts, getCategories } from "#utils/post";
+import { PAGE_SIZE, SMALL_PAGE_SIZE, SITE_DESCRIPTION, SITE_NAME } from "#constants";
+import styles from "./page.module.scss";
+import BlogBanner from "#components/BlogBanner";
 
 export const dynamic = "error";
 
@@ -16,25 +17,26 @@ const cx = classNames(styles, "home");
 export default async function Home() {
     const posts = getPosts();
     const categories = getCategories();
-    const devPosts = posts.filter(({ category }) => category === "/dev");
-    const postsInPage = devPosts.slice(0, PAGE_SIZE);
-    const galleryPosts = posts.filter(({ category }) => category === "/gallery").slice(0, 6);
+    const devPosts = posts.filter(({ category }) => category === "/dev").slice(0, PAGE_SIZE);
+    const galleryPosts = posts.filter(({ category }) => category === "/gallery").slice(0, SMALL_PAGE_SIZE);
 
     return (
         <section className={cx()}>
-            <header className={cx("__header")}>
-                <div className={cx("__header-content")}>
-                    <Typography variant="h1" fontWeight={700} marginBottom={8}>
-                        {SITE_NAME}
-                    </Typography>
-                    <Typography marginBottom={24}>{SITE_DESCRIPTION}</Typography>
-                </div>
-                <div className={cx("__profile")}>
-                    <Profile size="small" showContact />
-                </div>
-            </header>
+            <BlogBanner>
+                <header className={cx("__header")}>
+                    <div className={cx("__header-content")}>
+                        <Typography variant="h1" component="h1" fontWeight={700} marginBottom={8}>
+                            {SITE_NAME}
+                        </Typography>
+                        <Typography marginBottom={24}>{SITE_DESCRIPTION}</Typography>
+                    </div>
+                    <div className={cx("__profile")}>
+                        <Profile size="small" showContact />
+                    </div>
+                </header>
+            </BlogBanner>
             <section className={cx("__container")}>
-                <PostList posts={postsInPage} />
+                <PostList posts={devPosts} />
             </section>
             <section className={cx("__container", "__container--padding", "__category")}>
                 {categories
--- apps/blog/src/constants/lib/post.ts
+++ apps/blog/src/constants/lib/post.ts
@@ -4,3 +4,4 @@ export const POSTS_DIRECTORY = join(process.cwd(), "_posts");
 export const PUBLIC_DIRECTORY = join(process.cwd(), "public");
 
 export const PAGE_SIZE = 9;
+export const SMALL_PAGE_SIZE = 6;
