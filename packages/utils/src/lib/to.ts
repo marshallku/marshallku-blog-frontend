39078e4e955333eeacafc136c8f770e34026bffc
Marshall Ku <marshall@kakao.com>
2025-03-24 07:49:16 +0900

    Improve compatibility of to with handling none-promise values

--- packages/utils/src/lib/to.ts
+++ packages/utils/src/lib/to.ts
@@ -1,9 +1,22 @@
-// eslint-disable-next-line @rushstack/no-new-null
-export default async function to<T, E = Error>(promise: Promise<T>): Promise<[E, null] | [null, T]> {
-    try {
-        const data = await promise;
+export default function to<T, E = Error>(promise: Promise<T>): Promise<[E, null] | [null, T]>;
+export default function to<T, E = Error>(value: T): [E, null] | [null, T];
+export default function to<T, E = Error>(
+    promiseOrValue: Promise<T> | T,
+): Promise<[E, null] | [null, T]> | ([E, null] | [null, T]) {
+    if (promiseOrValue instanceof Promise) {
+        return promiseOrValue
+            .then<[null, T]>((data: T) => [null, data])
+            .catch<[E, null]>((error) => {
+                if (error instanceof Error) {
+                    return [error as E, null];
+                }
 
-        return [null, data];
+                return [{ message: "알 수 없는 오류가 발생했습니다" } as E, null];
+            });
+    }
+
+    try {
+        return [null, promiseOrValue];
     } catch (error) {
         if (error instanceof Error) {
             return [error as E, null];
