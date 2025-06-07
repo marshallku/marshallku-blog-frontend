c15b6c6016d0c706d69af86af78aeb7000ea9d08
Marshall Ku <marshall@kakao.com>
2025-06-07 20:42:02 +0900

    Fix api endpoints

--- apps/blog/src/api/comment/api.client.ts
+++ apps/blog/src/api/comment/api.client.ts
@@ -5,15 +5,15 @@ import { removeFalsy, removeNullish, stringifyQuery } from "@marshallku/utils";
 import { type GetRecentCommentParameters, type CommentListResponse, type CommentRequest } from "./types";
 
 export async function getComments(params: GetRecentCommentParameters = {}): Promise<CommentListResponse> {
-    return request<CommentListResponse>(`/comment/recent${stringifyQuery(removeNullish(params))}`);
+    return request<CommentListResponse>(`/api/v2/recent${stringifyQuery(removeNullish(params))}`);
 }
 
 export async function getCommentsBySlug(slug: string): Promise<CommentListResponse> {
-    return request<CommentListResponse>(`/comment/list?postSlug=${encodeURIComponent(slug)}`);
+    return request<CommentListResponse>(`/api/v2/comment/list?postSlug=${encodeURIComponent(slug)}`);
 }
 
 export async function postComment(data: CommentRequest) {
-    return request("/comment/create", {
+    return request("/api/v2/comment/create", {
         method: "POST",
         body: JSON.stringify(removeFalsy(data)),
     });
--- apps/blog/src/api/comment/api.server.ts
+++ apps/blog/src/api/comment/api.server.ts
@@ -5,7 +5,7 @@ import { removeFalsy } from "@marshallku/utils";
 import { type Comment, type CommentListResponse } from "./types";
 
 export async function getComments() {
-    return request<CommentListResponse>("/comment/recent", {
+    return request<CommentListResponse>("/api/v2/recent", {
         next: {
             revalidate: 3600,
         },
@@ -13,7 +13,7 @@ export async function getComments() {
 }
 
 export async function getCommentsBySlug(slug: string) {
-    return request<CommentListResponse>(`/comment/list?postSlug=${encodeURIComponent(slug)}`, {
+    return request<CommentListResponse>(`/api/v2/comment/list?postSlug=${encodeURIComponent(slug)}`, {
         next: {
             tags: [slug],
         },
@@ -21,7 +21,7 @@ export async function getCommentsBySlug(slug: string) {
 }
 
 export async function postComment(data: Omit<Comment, "_id" | "createdAt" | "byPostAuthor">) {
-    return request("/comment/create", {
+    return request("/api/v2/comment/create", {
         method: "POST",
         body: JSON.stringify(removeFalsy(data)),
     });
